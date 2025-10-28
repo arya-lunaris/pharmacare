0f19f2b009a501111d85f99f1b6eeb6e572f4b42
Arya Ram <arya_ram@outlook.com>
2025-10-28 07:13:37 +0000

    updated footer layout and sizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,32 @@
 function Footer() {
   return (
-    <footer className="bg-[#E3DFEC] mt-30 py-12">
-      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between space-y-12 md:space-y-0 md:space-x-12">
-        {/* Members of */}
-        <div className="flex flex-col items-center md:items-center space-y-2">
-          <h2 className="text-2xl font-bold text-[#5C427D] -mb-0">Members of:</h2>
-          <div className="flex flex-col items-center -space-y-4">
-            <img src="/images/gphc.png" alt="GPHC" className="h-35 w-35 object-contain -mb-10" />
-            <img src="/images/nhs.png" alt="NHS" className="h-20 w-20 object-contain -mb-8" />
-            <img src="/images/npa.png" alt="NPA" className="h-35 w-35 object-contain -mb-8" />
-            <img src="/images/registered.png" alt="Registered" className="h-20 w-20 object-contain mb-2" />
+    <footer className="bg-[#E3DFEC] mt-30 py-16 pb-8">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-center space-y-12 md:space-y-0 md:gap-x-50">
+        <div className="flex-1 flex flex-col items-center space-y-2">
+          {/* Members of */}
+          <h2 className="text-2xl font-normal text-[#5C427D] -mb-1.5">Members of:</h2>
+          <div className="flex flex-col items-center -space-y-6">
+            <img src="/images/gphc.png" alt="GPHC" className="h-28 w-28 object-contain" />
+            <img src="/images/nhs.png" alt="NHS" className="h-16 w-16 object-contain" />
+            <img src="/images/npa.png" alt="NPA" className="h-28 w-28 object-contain" />
+            <img src="/images/registered.png" alt="Registered" className="h-17 w-17 object-contain" />
           </div>
         </div>
 
-        {/* In partnership */}
-        <div className="flex flex-col items-center md:items-center space-y-2">
-          <h2 className="text-2xl font-bold text-[#5C427D] -mb-5">In partnership with:</h2>
-          <div className="flex flex-col items-center -space-y-4">
-            <img src="/images/camascope.jpg" alt="Camascope" className="h-40 w-40 object-contain -mb-15" />
-            <img src="/images/caremeds.jpg" alt="Caremeds" className="h-30 w-30 object-contain -mb-10" />
-            <img src="/images/atlas.png" alt="Atlas" className="h-35 w-35 object-contain -mb-20" />
-            <img src="/images/access.png" alt="Access" className="h-40 w-40 object-contain" />
+        <div className="flex-1 flex flex-col items-center space-y-2">
+          {/* In partnership */}
+          <h2 className="text-2xl font-normal text-[#5C427D] -mb-5">In partnership with:</h2>
+          <div className="flex flex-col items-center -space-y-10">
+            <img src="/images/camascope.jpg" alt="Camascope" className="h-32 w-32 object-contain" />
+            <img src="/images/caremeds.jpg" alt="Caremeds" className="h-24 w-24 object-contain -mb-7" />
+            <img src="/images/atlas.png" alt="Atlas" className="h-28 w-28 object-contain" />
+            <img src="/images/access.png" alt="Access" className="h-30 w-30 object-contain -mt-5" />
           </div>
         </div>
 
-        {/* Contact Information */}
-        <div className="flex flex-col items-center md:items-center space-y-4">
-          <h2 className="text-2xl font-bold text-[#5C427D] mb-7">Contact Information:</h2>
+        <div className="flex-1 flex flex-col items-center space-y-4">
+          {/* Contact Information */}
+          <h2 className="text-2xl font-normal text-[#5C427D] mb-7">Contact Information:</h2>
           <div className="flex flex-col items-center space-y-6">
             <div className="flex items-center space-x-3 text-lg">
               <img src="/images/address.png" alt="Address icon" className="h-6 w-6" />
@@ -47,10 +47,10 @@ function Footer() {
               </a>
             </div>
           </div>
-          <div className="mt-15 text-center">
-            <p className="text-gray-700 text-lg">&copy; 2025 Pharmacare. All rights reserved.</p>
-            <p className="text-gray-700 text-lg">GPhC Premises Reg No: 9011289</p>
-            <p className="text-gray-700 text-lg">Superintendent Pharmacist: Balbir Ram</p>
+          <div className="mt-10 text-center text-[14px]">
+            <p className="text-gray-700">&copy; 2025 Pharmacare. All rights reserved.</p>
+            <p className="text-gray-700">GPhC Premises Reg No: 9011289</p>
+            <p className="text-gray-700">Superintendent Pharmacist: Balbir Ram</p>
           </div>
         </div>
       </div>
